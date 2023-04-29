9ffe0a8afa6a4df7eb6a05a315cd699679e7adeb
Patricia <124640646+P-Trish@users.noreply.github.com>
2023-04-28 18:39:28 -0700

    relationships index.js

--- models/index.js
+++ models/index.js
@@ -1,2 +1,34 @@
 // DB RELATIONSHIPS
 
+const user = require('user');
+const blogpost = require('blogpost');
+const comment = require('comment');
+
+user.hasMany(blogpost, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+user.hasMany(comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+blogpost.belongsTo(user, {
+    foreignKey: 'user_id',
+});
+
+blogpost.hasMany(comment, {
+    foreignKey: 'blogpost_id',
+    onDelete: 'CASCADE',
+});
+
+comment.belongsTo(user, {
+    foreignKey: 'blogpost_id',
+});
+
+comment.belongsTo(blogpost, {
+    foreignKey: 'blogpost_id',
+});
+
+module.exports = {user, blogpost, comment};
